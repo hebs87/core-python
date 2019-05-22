c276e93d3d0db3eec63fcd37939ee386c0e61d89
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-05-22 09:20:45 +0000

    Installed Flask and redirected dependencies to .txt file

---
+++ pypi.py
@@ -0,0 +1,10 @@
+'''Importing OS library into .py file
+OS is built in Python library
+OS allows us to access operating system, as well as math, text and file handling modules'''
+import os
+
+'''Import Flask
+Flask is a Python framework and is external
+We need to us pip3 in CLI to install it first before is can be used
+Type sudo pip3 install Flask'''
+from flask import Flask
