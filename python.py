818e57b4d5a34f37358fc67a06488294bbeada47
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-05-22 08:52:49 +0000

    Created python.py file and added code to run in live interpreter

---
+++ python.py
@@ -0,0 +1,12 @@
+'''function to print a message, which will be used in the live interpreter'''
+def print_message(message):
+    print(message)
+
+# We call the function here
+print_message("Hello World!")
+
+'''list that we will be iterating over using a for loop'''
+my_list = [1, 2, 3, 4, 5]
+
+for item in my_list:
+    print(item)
